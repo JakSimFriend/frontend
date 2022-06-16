c2d58c05dee1bbddc6408461b4bf55056ad36713
Youngmin <gosmain@naver.com>
2022-06-16 18:46:00 +0900

    eslint & prettier setting

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
+  useTabs: false,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+};
